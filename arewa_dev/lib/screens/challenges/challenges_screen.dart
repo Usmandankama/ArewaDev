d5c6fa45d64616d3ecdbf8c4f25863bfe4220836
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-08-13 16:00:48 +0800

    Redesign challenges screen with new UI and fonts
    Updated the ChallengesScreen to use Google Fonts and flutter_screenutil for responsive design. Redesigned the UI with styled containers for HTML, CSS, and Build challenges, improved layout, and updated colors for better visual appeal.

--- arewa_dev/lib/screens/challenges/challenges_screen.dart
+++ arewa_dev/lib/screens/challenges/challenges_screen.dart
@@ -1,5 +1,7 @@
 import 'package:arewa_dev/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ChallengesScreen extends StatefulWidget {
   const ChallengesScreen({super.key});
@@ -14,9 +16,132 @@ class _ChallengesScreenState extends State<ChallengesScreen> {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
-        title: Text('Challenges'),
-        backgroundColor: AppColors.secondaryColor,
+        title: Center(
+          child: Text(
+            'Challenges',
+            style: GoogleFonts.montserrat(
+              color: AppColors.textPrimary,
+              fontSize: 20.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        backgroundColor: AppColors.backgroundColor,
         foregroundColor: AppColors.textPrimary,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 15.h),
+            
+              Container(
+                padding: EdgeInsets.all(20.r),
+                height: 180.h,
+                width: 330.w,
+                decoration: BoxDecoration(
+                  color: AppColors.accentBlue,
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                child: Column(
+                  
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      textAlign: TextAlign.left,
+                      'HTML Challenges',
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.textPrimary,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      'Test your HTML skills with interactive challenges.',
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.textSecondary,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Container(
+                padding: EdgeInsets.all(20.r),
+                height: 180.h,
+                width: 330.w,
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                child: Column(
+                  
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      textAlign: TextAlign.left,
+                      'Css Challenges',
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.textPrimary,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      'Test your CSS skills with interactive challenges.',
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.textSecondary,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Container(
+                padding: EdgeInsets.all(20.r),
+                height: 180.h,
+                width: 330.w,
+                decoration: BoxDecoration(
+                  color: AppColors.accentPurple,
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                child: Column(
+                  
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      textAlign: TextAlign.left,
+                      'Build Challenges',
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.textPrimary,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      'Test your skills with quick, interactive HTML & CSS challenges.',
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.textSecondary,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.h),
+            ],
+          ),
+        ),
       ),
     );
   }
