72be41bdc614311bc1cba5deab40a6f66f8e9273
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-08-23 05:02:26 +0800

    Comment out ContentModel class and import
    The ContentModel class and its related import have been commented out, possibly for deprecation, refactoring, or to temporarily disable its usage.

--- arewa_dev/lib/models/content_model.dart
+++ arewa_dev/lib/models/content_model.dart
@@ -1,24 +1,24 @@
-import 'package:arewa_dev/models/example_model.dart';
+// import 'package:arewa_dev/models/example_model.dart';
 
-class ContentModel {
-  final int id;
-  final String title;
-  final String content;
-  final ExampleModel example;
+// class ContentModel {
+//   final int id;
+//   final String title;
+//   final String content;
+//   final ExampleModel example;
 
-  ContentModel({
-    required this.id,
-    required this.title,
-    required this.content,
-    required this.example,
-  });
+//   ContentModel({
+//     required this.id,
+//     required this.title,
+//     required this.content,
+//     required this.example,
+//   });
 
-  factory ContentModel.fromJson(Map<String, dynamic> json) {
-    return ContentModel(
-      id: json['id'],
-      title: json['title'],
-      content: json['content'],
-      example: ExampleModel.fromJson(json['example']),
-    );
-  }
-}
+//   factory ContentModel.fromJson(Map<String, dynamic> json) {
+//     return ContentModel(
+//       id: json['id'],
+//       title: json['title'],
+//       content: json['content'],
+//       example: ExampleModel.fromJson(json['example']),
+//     );
+//   }
+// }
